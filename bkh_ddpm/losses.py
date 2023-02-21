fixed issue with loss calculation

bkh_ddpm/losses.py
@@ -44,7 +44,7 @@ def approx_standard_normal_cdf(x):
     A fast approximation of the cumulative distribution function of the
     standard normal.
     """
-    return 0.5 * (1.0 + torch.tanh(np.sqrt(2.0 / np.pi) * (x + 0.044715 * torch.pow(x, 3))))
+    return 0.5 * (1.0 + torch.tanh(torch.sqrt(2.0 / torch.tensor(np.pi)) * (x + 0.044715 * torch.pow(x, 3))))
 
 
 def discretized_gaussian_log_likelihood(x, *, means, log_scales):