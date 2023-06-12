39322df8e56ce1974f5093cf869cc1a6ff3393a7
Bardia Khosravi <m253231@ROQORTH001-GPU02.mayo.edu>
2023-06-11 20:18:27 -0500

    debug

--- bkh_ddpm/solvers.py
+++ bkh_ddpm/solvers.py
@@ -125,9 +125,10 @@ class DDIMSolver(SolverBase):
 
         _, self.use_timesteps = get_respaced_betas(base_diffusion.betas, f"DDIM{num_steps}")
         if base_diffusion.num_timesteps - 1 not in self.use_timesteps:
-            self.use_timesteps.append(base_diffusion.num_timesteps - 1)
-            self.use_timesteps = sorted(self.use_timesteps)
-        
+            self.use_timesteps = torch.cat(self.use_timesteps, torch.tensor([base_diffusion.num_timesteps - 1]))
+            import warnings
+            warnings.warn(f"{self.use_timesteps} does not include the last timestep.}")
+                    
         self.timestep_map = []
         self.original_num_steps = len(kwargs["betas"])
 
