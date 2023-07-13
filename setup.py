f96a6311dd27202610495183133f332a5fecd920
Bardia Khosravi <m253231@ROQORTH001-GPU02.mayo.edu>
2023-07-13 15:31:17 -0500

    fixed issue with device selection with cls-cond

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="bkh_ddpm",
-    version="0.3.9",
+    version="0.3.10",
     author="Bardia Khosravi",
     author_email="bardiakhosravi95@gmail.com",
     description="Personal take on DDPMs",
