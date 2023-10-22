b4e0093e37f29ee637ce29875491cf8634ba354c
Bardia Khosravi <bardiakhosravi95@gmail.com>
2023-10-22 12:43:24 -0500

    version update

--- setup.py
+++ setup.py
@@ -6,27 +6,26 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="mediffusion",
-    version="0.6.0",
+    version="0.6.2",
     author="Bardia Khosravi",
     author_email="bardiakhosravi95@gmail.com",
     description="Diffusion Models for Medical Imaging",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/BardiaKh/Mediffusion",
-    packages = setuptools.find_packages(where="mediffusion"),
+    packages = setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     install_requires=[
-        "bkh-pytorch-utils==0.9.0",
+        "bkh-pytorch-utils==0.9.2",
         "torchextractor==0.3.0",
         "OmegaConf==2.0.0",
     ],
     package_data={
         'mediffusion': ['./default_config/default.yaml'],
     },
-    package_dir = {"": "mediffusion"},
     python_requires='>=3.8',
 )
